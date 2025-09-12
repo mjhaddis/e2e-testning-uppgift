3b102320146da309e8ad9153aa3721bfe4230e9c
Michael <mjhaddis@gmail.com>
2025-09-12 23:03:24 +0200

    2 1/2 test done

--- cypress/e2e/happy-flow.cy.ts
+++ cypress/e2e/happy-flow.cy.ts
@@ -1,5 +1,25 @@
-describe("Fetching data", () => {
+describe("Movie search tests", () => {
   beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should have no movies from start", () => {
+    cy.get("div#movie-container").children().should("have.length", 0);
+  });
+
+  it("should display message from empty/invalid input", () => {
+    const form = cy.get("form#searchForm").should("exist");
+    const input = cy.get("input#searchText").should("exist");
+    const button = cy.get("button#search").should("exist");
+
+    button.click();
+
+    cy.get("div#movie-container").children().should("have.length.at.least", 1);
+  });
+
+  it("should display data from movie API", () => {
+    cy.intercept("GET", "**/search").as("search");
+    cy.get("#searchText").type("matrix");
+    cy.get("#search").click();
   });
 });
