52330b36df791a13d5f1ebb47fb9eb799a582272
Michael <mjhaddis@gmail.com>
2025-09-13 14:13:11 +0200

    added mocked test

--- cypress/e2e/happy-flow.cy.ts
+++ cypress/e2e/happy-flow.cy.ts
@@ -8,13 +8,35 @@ describe("Movie search tests", () => {
   });
 
   it("should display message from empty/invalid input", () => {
-    const form = cy.get("form#searchForm").should("exist");
-    const input = cy.get("input#searchText").should("exist");
+    cy.get("form#searchForm").should("exist");
+    cy.get("input#searchText").should("exist");
     const button = cy.get("button#search").should("exist");
 
     button.click();
 
     cy.get("div#movie-container").children().should("have.length.at.least", 1);
+  });
+
+  it("should show mocked movie", () => {
+    cy.intercept("GET", "**/omdbapi.com/**", {
+      statusCode: 200,
+      body: {
+        Search: [{ Title: "The Matrix", Year: "1999" }],
+        totalResults: "1",
+        Response: "True",
+      },
+    });
+
+    cy.get("#searchText").type("Matrix");
+
+    const button = cy.get("button#search").should("exist");
+    button.click();
+
+    cy.get("#movie-container").children().should("have.length", 1);
+    cy.get("#movie-container")
+      .children()
+      .first()
+      .should("have.text", "The Matrix");
   });
 
   it("should display data from movie API", () => {
